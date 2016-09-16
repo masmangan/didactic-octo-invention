164070e78a25113f4b938445765c4aa7c5e7e44b
Marco Mangan <marco.mangan@pucrs.br>
2016-09-15 21:40:38 -0300

    Criar conta #2

---
+++ transacoes/src/main/java/ContaCorrente.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * @author marco.mangan@pucrs.br
+ */
+public class ContaCorrente {
+    private int numero;
+    private double saldo;
+
+    public ContaCorrente(int numero, double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaCorrente{" + "numero=" + numero + ", saldo=" + saldo + '}';
+    }
+    
+}
